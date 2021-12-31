634d61d7070c39046d39f101787ebf578ed40884
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-12-31 14:44:53 +0200

    api: change ATLAS_CONNECTION_STRING

--- api/src/config/constants.js
+++ api/src/config/constants.js
@@ -2,7 +2,7 @@ const { ATLAS_CONNECTION_STRING } = require("./constants-ports/constant");
 
 exports.PORT = process.env.PORT || 5000;
 // if you havent ATLAS_CONNECTION_STRING you can clear this connection string: 
-exports.DB_CONNECTION_STRING = ATLAS_CONNECTION_STRING; // and:
+exports.DB_CONNECTION_STRING = 'mongodb+srv://Gogo:GGDZaHTTVnU35Xu3@test.g6uli.mongodb.net/rentAuto?retryWrites=true&w=majority'; // and:
 // exports.DB_CONNECTION_STRING = 'mongodb://localhost:27017/rent-auto';
 
 exports.JWT_SECRET = 'asd123gfgASD44dsfsgfASDJJLFDPL233454ds';
